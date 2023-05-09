1882dd681679e0134bfa2c2bbc35fd0f0bc98d6f
Bruno Dias <ibroopi@gmail.com>
2023-05-09 14:13:46 +0000

    Initial commit

---
+++ Plugin/Catalog/Block/Toolbar.php
@@ -0,0 +1,35 @@
+<?php
+namespace OuterEdge\CategoryFilters\Plugin\Catalog\Block;
+
+use Magento\Catalog\Block\Product\ProductList\Toolbar as CoreToolbar;
+
+class Toolbar
+{
+
+    /**
+    * @param CoreToolbar $subject
+    * @param \Closure $proceed
+    * @param \Magento\Framework\Data\Collection $collection
+    * @return \Magento\Catalog\Block\Product\ProductList\Toolbar
+    */
+    public function aroundSetCollection(CoreToolbar $subject, \Closure $proceed, $collection)
+    {
+        $currentOrder = $subject->getCurrentOrder();
+        $result = $proceed($collection);
+
+        if ($currentOrder) {
+
+            switch ($currentOrder) {
+                case 'price_desc':
+                    $subject->getCollection()->setOrder('price', 'desc');
+                    break;
+                case 'created_at_desc':
+                    $subject->getCollection()->setOrder('created_at', 'desc');
+                    break;
+            }
+        }
+
+        return $result;
+    }
+
+}
