c8cb794d2c4a8bef5d925258fa110b0803b3d401
Bruno Dias <ibroopi@gmail.com>
2025-08-28 11:13:36 +0100

    Split crons

--- Cron/SetInStockSearch.php
+++ Cron/SetInStockSearch.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace OuterEdge\CategoryFilters\Model;
+namespace OuterEdge\CategoryFilters\Cron;
 
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\Catalog\Model\ResourceModel\Product\Action;
@@ -8,22 +8,13 @@ use Psr\Log\LoggerInterface;
 use Magento\Indexer\Model\IndexerFactory;
 use Magento\InventorySalesAdminUi\Model\GetSalableQuantityDataBySku;
 use Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable;
-use Magento\CatalogInventory\Api\StockRegistryInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\CatalogInventory\Model\Stock\StockItemRepository;
 use Magento\Catalog\Model\Product\Attribute\Source\Status;
 use Magento\Catalog\Model\Product\Type;
 use OuterEdge\CategoryFilters\Helper\Data;
-use Magento\Framework\Stdlib\DateTime\DateTime;
-use Magento\Framework\App\ResourceConnection;
-use Magento\Framework\App\State;
-use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Store\Model\ScopeInterface;
 
-class Cron
+class SetInStockSearch
 {
-    CONST BEST_SELLING_RANGE = 30;
-
     protected $indexerIds = array(
         'catalog_category_product',
         'catalog_product_category',
@@ -34,8 +25,6 @@ class Cron
         'catalogsearch_fulltext',
     );
 
-    protected $bestSellingRange;
-
     public function __construct(
         protected CollectionFactory $productCollectionFactory,
         protected Action $action,
@@ -43,83 +32,13 @@ class Cron
         protected IndexerFactory $indexerFactory,
         protected GetSalableQuantityDataBySku $stockState,
         protected Configurable $configModel,
-        protected StockRegistryInterface $stockRegistryInterface,
         protected ProductRepositoryInterface $productRepositoryInterface,
-        protected StockItemRepository $stockItemRepository,
-        protected Data $helper,
-        protected DateTime $dateTime,
-        protected ResourceConnection $resourceConnection,
-        protected State $state,
-        protected ScopeConfigInterface $scopeConfig
+        protected Data $helper
     )
     {
-        $this->bestSellingRange = $this->scopeConfig->getValue(
-            'oe_category_filter/settings/best_selling_range',
-            ScopeInterface::SCOPE_STORE
-        ) ?? self::BEST_SELLING_RANGE;
     }
-
-    private function getBestSellingProductsCollection()
-    {
-        $to = $this->dateTime->gmtDate('Y-m-d');
-        $from = $this->dateTime->gmtDate('Y-m-d', strtotime("-{$this->bestSellingRange} days"));
-
-        $connection = $this->resourceConnection->getConnection();
-        $select = $connection->select()
-            ->from(
-                ['main_table' => $this->resourceConnection->getTableName('sales_bestsellers_aggregated_monthly')],
-                [
-                    'product_id',
-                    'rating_pos'
-                ]
-            )
-            ->where('period <= ?', $to)
-            ->where('period >= ?', $from)
-            ->group('main_table.product_id')
-            ->limit(100);
-
-        return $connection->fetchAll($select);
-    }
-
-    public function setPopularity()
-    {
-        $this->logger->info('Running setPopularity cron job');
-
-        if (!$this->helper->isPopularityEnabled()) {
-            return;
-        }
-        try {
-            $productIdsToIndex = [];
-
-            foreach ($this->getBestSellingProductsCollection() as $item) {
-                $product = $this->productRepositoryInterface->getById($item['product_id']);
-
-                if ($product->getPopularity() != $item['rating_pos']) {
-                    $this->action->updateAttributes(
-                        [$product->getId()],
-                        ['popularity' => $item['rating_pos']],
-                        $product->getStoreId());
-                    $productIdsToIndex[] = $product->getId();
-                }
-            }
-
-            //Perform bulk re-indexing
-            $productIdsToIndex = array_unique($productIdsToIndex);
-            if (!empty($productIdsToIndex)) {
-                foreach ($this->indexerIds as $indexerId) {
-                    $indexer = $this->indexerFactory->create()->load($indexerId);
-                    $indexer->reindexList($productIdsToIndex);
-                }
-            }
-        } catch (\Exception $e) {
-            $this->logger->critical('Error running setPopularity cron job: ' . $e->getMessage());
-        }
-        $this->logger->info('Finished setPopularity cron job');
-
-        return;
-    }
-
-    public function setInStockSearch()
+    
+    public function execute()
     {
         $this->logger->info('Running setInStockSearch cron job');
         if (!$this->helper->isInStockFirstEnabled()) {
